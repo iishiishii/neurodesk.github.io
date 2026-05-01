6801198fa35070d91bc426b71ecea5f876287299
Steffen Bollmann <stebo85@users.noreply.github.com>
2026-05-01 15:13:42 -0700

    remove owners as it is not allowed to run interactive on there

--- content/en/Getting-Started/Installations/connectSherlock.sh
+++ content/en/Getting-Started/Installations/connectSherlock.sh
@@ -348,7 +348,6 @@ function connectSherlock() {
     echo " normal      || 2d or 7d (owner) |      8GB  | 20-64"
     echo " bigmem      ||               1d |     64GB  | 24-256"
     echo " dev         ||               2h |      8GB  | 20-32"
-    echo " owners      || preemptible owner nodes; use for jobs that can get interrupted"
     echo " <sunetid>   || owner partition, usually the PI's SUNet ID"
     echo "--------------------------------------------------------------------------------"
     echo "================================================================================"
